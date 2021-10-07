8048cd51be421a05d907c8e9f58e97ec55d70133
JoaoHAraujo <j.henr@outlook.com>
2021-10-06 21:24:35 -0300

    auto preenchimento api

--- script.js
+++ script.js
@@ -4,17 +4,15 @@ const optSexoOutros = document.querySelector('#optSexoOutros')
 const btnEnviar = document.querySelector('input[type="submit"]')
 
 
-function paraMaiusculo(){
+function paraMaiusculo(){ // Padroniza os dados do form colocando em maiusculo dados não sensiveis
     const inputMaiusculo = document.querySelectorAll('.maiusculo')
     inputMaiusculo.forEach((input) => {
-        input.addEventListener('change', () => {
-            input.value = input.value.toUpperCase()
-        })  
+        input.value = input.value.toUpperCase()
     })
 }
 
 
-function habilitaSexoOutros() {
+function habilitaSexoOutros() { // Habilita inserção manual de outras opções pra sexo
     optSexo.forEach((opt) => {
         opt.addEventListener('change', () => {
             if (optSexoOutros.checked) {
@@ -27,15 +25,38 @@ function habilitaSexoOutros() {
     })
 }
 
-function enviarForm() {
-    btnEnviar.addEventListener('click', () => {
-        if (optSexoOutros.checked) {
-            optSexoOutros.value = txtSexoOutros.value
-            txtSexoOutros.disabled = true
-        }
-    })
+const inputCEP = document.querySelector('#cep')
+
+inputCEP.addEventListener('change', () => {
+    preencheEndereco(parseInt(inputCEP.value))
+})
+
+async function preencheEndereco(cep) { // Busca na API as informações de endereço a partir do CEP e preenche form
+    let res = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    let endereco = await res.json()
+
+    const logradouro = document.querySelector('#logradouro')
+    const bairro = document.querySelector('#bairro')
+    const cidade = document.querySelector('#cidade')
+    const uf = document.querySelector('#uf')
+
+    logradouro.value = endereco.logradouro
+    bairro.value = endereco.bairro
+    cidade.value = endereco.localidade
+    uf.value = endereco.uf  
 }
 
-paraMaiusculo()
+
+function enviarForm() {
+    btnEnviar.addEventListener('click', () => {
+        paraMaiusculo()
+        if (optSexoOutros.checked) {
+            optSexoOutros.value = txtSexoOutros.value // Transfere dados do input de texto para a opção "Outros" padronizando dados
+            txtSexoOutros.disabled = true // Evita que o dado seja duplicado desabilitando o input antes de enviar dados
+        }
+    })
+    
+}
+
 habilitaSexoOutros()
 enviarForm()
